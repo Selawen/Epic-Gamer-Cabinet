69c33f3d52fcc7b15299d3a993eaa99da2d7acce
Mana <manakouwenberg@gmail.com>
2021-04-16 11:17:07 +0200

    Initialized a Unity project to prototype in and added the ManaBasis to it

---
+++ Vrij_II_Prototype/Vrij_II_Prototype/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    public UnityEvent deathEvents, winEvents;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    #region ManaBasic Functionality
+    public void Die()
+    {
+        deathEvents.Invoke();
+    }
+
+    public void Win()
+    {
+        winEvents.Invoke();
+    }
+    #endregion
+
+    #region debug
+
+    #endregion
+}
---
+++ Vrij_II_Prototype/Vrij_II_Prototype/Assets/Scripts/ProjectileMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public void Init(float speed)
+    {
+        moveSpeed = speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+    }
+}
