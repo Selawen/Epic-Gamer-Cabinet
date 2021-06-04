e710446fcfeb40063bcdb83a9ed20666702d1ed8
Selawen <37039183+Selawen@users.noreply.github.com>
2021-06-04 12:47:27 +0200

    Added example for inputs
    - added stick control to arcade inputs script
    - added example script and scene for inputs

--- Kabinet Vrij II/Assets/General Assets/Scripts/ArcadeInputs.cs
+++ Kabinet Vrij II/Assets/General Assets/Scripts/ArcadeInputs.cs
@@ -7,27 +7,80 @@ public static class ArcadeInputs
 {
     public static bool Red()
     {
-        return Joystick.current["button5"].IsPressed() || Input.GetKeyDown(KeyCode.J);
+        if (Joystick.current == null) { return Input.GetKey(KeyCode.J); }
+        return Joystick.current["button5"].IsPressed() || Input.GetKey(KeyCode.J);
     }
 
     public static bool Green()
     {
-        return Joystick.current["button2"].IsPressed() || Input.GetKeyDown(KeyCode.I);
+        if (Joystick.current == null) { return Input.GetKey(KeyCode.I); }
+
+        return Joystick.current["button2"].IsPressed() || Input.GetKey(KeyCode.I);
     }
 
     public static bool Blue()
     {
-        return Joystick.current["button3"].IsPressed() || Input.GetKeyDown(KeyCode.K);
+        if (Joystick.current == null) { return Input.GetKey(KeyCode.K); }
+
+        return Joystick.current["button3"].IsPressed() || Input.GetKey(KeyCode.K);
     }
 
     public static bool Yellow()
     {
-        return Joystick.current["button4"].IsPressed() || Input.GetKeyDown(KeyCode.U);
+        if (Joystick.current == null) { return Input.GetKey(KeyCode.U); }
+
+        return Joystick.current["button4"].IsPressed() || Input.GetKey(KeyCode.U);
     }
 
     public static bool Select()
     {
-        return Joystick.current["button12"].IsPressed() || Input.GetKeyDown(KeyCode.Space);
+        if (Joystick.current == null) { return Input.GetKey(KeyCode.Space); }
+
+        return Joystick.current["button12"].IsPressed() || Input.GetKey(KeyCode.Space);
     }
 
+    public static Vector2 Stick()
+    {
+        if (Joystick.current == null)
+        {
+            return new Vector2(0, 0);
+        }
+        return Joystick.current.stick.ReadValue();
+    }
+
+    public static bool StickUp()
+    {
+        if (Joystick.current == null)
+        {
+            return false;
+        }
+        return (Joystick.current.stick.ReadValue().y == 1);
+    }
+
+    public static bool StickDown()
+    {
+        if (Joystick.current == null)
+        {
+            return false;
+        }
+        return (Joystick.current.stick.ReadValue().y == -1);
+    }
+
+    public static bool StickLeft()
+    {
+        if (Joystick.current == null)
+        {
+            return false;
+        }
+        return (Joystick.current.stick.ReadValue().x == -1);
+    }
+
+    public static bool StickRight()
+    {
+        if (Joystick.current == null)
+        {
+            return false;
+        }
+        return (Joystick.current.stick.ReadValue().x == 1);
+    }
 }
